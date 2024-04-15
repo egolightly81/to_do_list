4c909ffe1b9a3b5f9aaa0a45e1196d43bec8c380
Emerald <140569368+egolightly81@users.noreply.github.com>
2024-04-15 09:28:29 +0800

    changes to event handler in JS - task name

--- js/scripts.js
+++ js/scripts.js
@@ -5,12 +5,20 @@ console.log("script Loaded");
     let addTaskButton = document.getElementById("add-button")
     let newTaskInput = document.getElementById("task-input")
     let todoListContainer = document.getElementById("todo-list")
+    let templateElement = document.getElementById("todo-item-template")
+    let template = templateElement.innerHTML
     
     /* Step 2. Lets write the function to handle the 'click' event
     ---------------------------------------------------------------*/
     function onAddTaskClicked(event){
+        // Get the contents of the input box
         let taskName = newTaskInput.value;
+        // clear the input box 
         newTaskInput.value = "";
+        // Search and replace to add task name (found above)
+        let = todoHTML = template.replace("<!----TASK_NAME---->", taskName)
+
+        // we have the formatted HML, lets insert it into the to do container 
         todoListContainer.insertAdjacentHTML('afterbegin', taskname);
     }
 
